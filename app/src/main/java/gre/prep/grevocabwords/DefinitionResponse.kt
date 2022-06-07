038f34832344cbd72c38e6911a60ada4fe409a45
zohaib <zohaibansari100@gmail.com>
2022-06-07 22:17:45 +0530

    update UI

--- app/src/main/java/gre/prep/grevocabwords/DefinitionResponse.kt
+++ app/src/main/java/gre/prep/grevocabwords/DefinitionResponse.kt
@@ -2,25 +2,25 @@ package gre.prep.grevocabwords
 
 data class DefinitionResponseItem(
     val meanings: List<Meaning>,
-    val phonetic: String,
+    val phonetic: String?,
     val phonetics: List<Phonetic>,
     val sourceUrls: List<String>,
-    val word: String
+    val word: String?
 )
 data class Meaning(
     val antonyms: List<String>,
     val definitions: List<Definition>,
-    val partOfSpeech: String,
+    val partOfSpeech: String?,
     val synonyms: List<String>
 )
 
 data class Definition(
     val antonyms: List<String>,
-    val definition: String,
-    val example: String,
+    val definition: String?,
+    val example: String?,
     val synonyms: List<String>
 )
 
 data class Phonetic(
-    val text: String,
+    val text: String?,
 )
--- app/src/main/java/gre/prep/grevocabwords/ui/WordPage.kt
+++ app/src/main/java/gre/prep/grevocabwords/ui/WordPage.kt
@@ -1,14 +1,30 @@
 package gre.prep.grevocabwords.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import gre.prep.grevocabwords.*
+import gre.prep.grevocabwords.DefinitionResponseItem
+import gre.prep.grevocabwords.Resource
+import gre.prep.grevocabwords.WordData
+import gre.prep.grevocabwords.WordDetailsViewModel
 
 @Composable
 fun WordDetailsPage(
@@ -44,17 +60,46 @@ fun WordDetailsPage(
 fun WordDetails(data: List<DefinitionResponseItem>?) {
     //show error view ideally
     data ?: return
-    
-    Column {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .verticalScroll(scrollState)
+            .fillMaxSize()
+    ) {
         data.forEach { currentWord ->
             Column {
-                Text(text = currentWord.word)
+                Text(
+                    text = buildAnnotatedString {
+                        append(currentWord.word + "   ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Blue,
+                                fontWeight = FontWeight.Light,
+                                fontStyle = FontStyle.Italic,
+                                fontSize = 24.sp,
+                            )
+                        ) {
+                            append((currentWord.phonetic ?: ""))
+                        }
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    fontSize = 28.sp,
+                    style = MaterialTheme.typography.h1,
+                )
+                Divider()
                 currentWord.meanings.forEach { meaning ->
-                    meaning.definitions.forEach { currentDefinition ->
-                        Text(text = currentDefinition.definition)
+                    meaning.definitions.forEachIndexed { index, currentDefinition ->
+                        Text(
+                            text = (index + 1).toString() + ". " + currentDefinition.definition,
+                            fontSize = 18.sp,
+                            style = MaterialTheme.typography.caption,
+                            modifier = Modifier.padding(horizontal = 6.dp, vertical = 8.dp)
+                        )
                         SynonymRow(currentDefinition.synonyms)
-                        SynonymRow(currentDefinition.antonyms)
+                        if(index == meaning.definitions.lastIndex && currentDefinition.synonyms.isNotEmpty()) Spacer(modifier = Modifier.height(8.dp))
                     }
+                    Divider()
                 }
             }
         }
@@ -63,9 +108,25 @@ fun WordDetails(data: List<DefinitionResponseItem>?) {
 
 @Composable
 fun SynonymRow(words: List<String>) {
-    Row {
-       words.forEach { word ->
-           Text(text = word)
-       }
+    val scrollState = rememberScrollState()
+    Row(
+        modifier = Modifier
+            .horizontalScroll(scrollState)
+            .padding(start = 20.dp)
+    ) {
+        words.forEach { word ->
+            Text(
+                text = word,
+                fontSize = 18.sp,
+                style = MaterialTheme.typography.caption,
+                modifier = Modifier
+                    .border(
+                        border = BorderStroke(width = 1.dp, color = Color.Blue),
+                        shape = CircleShape
+                    )
+                    .padding(horizontal = 8.dp, vertical = 3.dp),
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+        }
     }
 }
